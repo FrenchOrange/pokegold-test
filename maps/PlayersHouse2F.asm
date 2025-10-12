49454c6ab9f592b84de166ef4175b3caedf05428
FrenchOrange <64930003+FrenchOrange@users.noreply.github.com>
2025-10-13 00:16:24 +0200

    Add debug setup

--- maps/PlayersHouse2F.asm
+++ maps/PlayersHouse2F.asm
@@ -110,6 +110,66 @@ PlayersRadioText4:
 	line "#MON CHANNEL…"
 	done
 
+DebugSign:
+	setflag ENGINE_POKEDEX
+	setflag ENGINE_FLYPOINT_NEW_BARK
+	setflag ENGINE_FLYPOINT_CHERRYGROVE
+	setflag ENGINE_FLYPOINT_VIOLET
+	setflag ENGINE_FLYPOINT_AZALEA
+	setflag ENGINE_FLYPOINT_GOLDENROD
+	setflag ENGINE_FLYPOINT_ECRUTEAK
+	setflag ENGINE_ZEPHYRBADGE
+	setflag ENGINE_HIVEBADGE
+	setflag ENGINE_PLAINBADGE
+	setflag ENGINE_FOGBADGE
+	setflag ENGINE_MINERALBADGE
+	setflag ENGINE_STORMBADGE
+	setflag ENGINE_GLACIERBADGE
+	setflag ENGINE_RISINGBADGE
+	giveitem MASTER_BALL, 99
+	giveitem RARE_CANDY, 99
+	giveitem MAX_REPEL, 99
+	giveitem ESCAPE_ROPE, 99
+	giveitem TOWN_MAP
+	giveitem BICYCLE
+	giveitem OLD_ROD
+	giveitem GOOD_ROD
+	giveitem SUPER_ROD
+	giveitem HM_CUT
+	giveitem HM_FLY
+	giveitem HM_SURF
+	giveitem HM_STRENGTH
+	giveitem HM_FLASH
+	giveitem HM_WHIRLPOOL
+	giveitem HM_WATERFALL
+	giveitem TM_ROCK_SMASH, 99
+	giveitem TM_DIG, 99
+	opentext
+; debug team
+	givepoke FERALIGATR, 100
+	givepoke PIDGEOT, 100
+	givepoke LAPRAS, 100
+	givepoke PIKACHU, 100
+	givepoke DIGLETT, 100
+	givepoke DITTO, 100
+; debug hm
+	loadmem wPartyMon1Moves+0, SURF
+	loadmem wPartyMon1Moves+1, ROCK_SMASH
+	loadmem wPartyMon2Moves+0, FLY
+	loadmem wPartyMon2Moves+1, STRENGTH
+	loadmem wPartyMon2Moves+2, CUT
+	loadmem wPartyMon3Moves+2, WATERFALL
+	loadmem wPartyMon4Moves+0, FLASH
+	loadmem wPartyMon4Moves+1, HEADBUTT
+	closetext
+;	setevent EVENT_GOT_RUNNING_SHOES
+;	setmapscene PLAYERS_HOUSE_1F, SCENE_PLAYERSHOUSE1F_NOOP
+	playsound SFX_WARP_TO
+	special FadeOutToBlack
+	waitsfx
+	warp VIOLET_CITY, 31, 26
+	end
+
 PlayersHouse2F_MapEvents:
 	db 0, 0 ; filler
 
@@ -123,6 +183,7 @@ PlayersHouse2F_MapEvents:
 	bg_event  3,  1, BGEVENT_READ, PlayersHouseRadioScript
 	bg_event  5,  1, BGEVENT_READ, PlayersHouseBookshelfScript
 	bg_event  6,  0, BGEVENT_IFSET, PlayersHousePosterScript
+	bg_event  1,  0, BGEVENT_READ, DebugSign
 
 	def_object_events
 	object_event  4,  2, SPRITE_CONSOLE, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, PlayersHouseGameConsoleScript, EVENT_PLAYERS_HOUSE_2F_CONSOLE
