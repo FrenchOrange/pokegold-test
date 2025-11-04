5e396689fcd270e41a71e51829b5475e9cc3b755
FrenchOrange <64930003+FrenchOrange@users.noreply.github.com>
2025-11-04 18:00:27 +0100

    Fix item prices

--- maps/WillsRoom.asm
+++ maps/WillsRoom.asm
@@ -139,4 +139,4 @@ WillsRoom_MapEvents:
 	def_bg_events
 
 	def_object_events
-	object_event  5,  7, SPRITE_SHAUNTAL, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_SCRIPT, 0, WillScript_Battle, -1
+	object_event  5,  7, SPRITE_SHAUNTAL, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, WillScript_Battle, -1
